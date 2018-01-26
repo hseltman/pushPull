690a2f8cf21be4125650cdb5a0d59b4cd8562cf2
Howard Seltman <hseltman@stat.cmu.edu>
2018-01-26 09:09:50 -0500

    first full implementation of push() and pull()

--- pushPull.py
+++ pushPull.py
@@ -13,7 +13,8 @@ import pysftp
 import os.path
 
 
-def pull(files, who=None):
+def push(files):
+    """ Upload files to sftp server.  See pushPull.R for more details. """
     # Check input
     if isinstance(files, str):
         files = [files]
@@ -21,6 +22,13 @@ def pull(files, who=None):
         raise TypeError("'files' must be a str or list of str's")
     if not all([isinstance(s, str) for s in files]):
         raise TypeError("all 'files' elements must be str's")
+
+    # Check if files are available
+    OK = [os.path.isfile(f) for f in files]
+    if not all(OK):
+        print("Missing files:",
+              ", ".join([f for (f, o) in zip(files, OK) if not o]))
+        return "Failed"
 
     # Get configuration info
     cDir = os.path.expanduser("~/pushPullConfig.csv")
@@ -36,12 +44,12 @@ def pull(files, who=None):
             config = fh.read()
     except FileNotFoundError:
         [print(m) for m in msg]
-        return None
+        return "Failed"
 
     config = [line.split(',') for line in config.splitlines()]
     if not all(len(e) == 2 for e in config):
         [print(m) for m in msg]
-        return None
+        return "Failed"
 
     config = {k.strip(): v.strip() for (k, v) in config}
     needed = ('sftpSite', 'sftpName', 'sftpPassword', 'userName')
@@ -73,61 +81,142 @@ def pull(files, who=None):
                 sftp.cwd(startDir)
         except FileNotFoundError:
             print("the starting folder in 'sftpSite' is wrong")
-            return None
+            return "Failure"
     except pysftp.ConnectionException:
         print("Cannot connect to {}".format(site))
-        return None
+        return "Failure"
     except pysftp.AuthenticationException:
         print("Bad username or password in configuration file")
-        return None
+        return "Failure"
+
+    # Change to user's folder, creating it if needed
+    if config['userName'] not in ['', '.']:
+        if not sftp.isdir(config['userName']):
+            try:
+                sftp.mkdir(config['userName'])
+            except Exception:
+                print("Cannot make folder", config['userName'])
+                return "Failure"
+        try:
+            sftp.chdir(config['userName'])
+        except IOError:
+            print("Cannot chdir server to", config['userName'])
+            return("Failure")
+
+    # push the files
+    fail_count = 0
+    for f in files:
+        try:
+            sftp.put(f)
+        except IOError:
+            print("cannot put {} on server".format(f))
+            fail_count += 1
+        except OSError:
+            print("cannot find {} on your computer".format(f))
+            fail_count += 1
+    sftp.close()
+
+    if fail_count == len(files):
+        return "Failed"
+    if fail_count == 0:
+        return "Success"
+    return "Partial success"
+
+
+def pull(files, who=None):
+    """ Download files from sftp server.  See pushPull.R for more details. """
+    # Check input
+    if isinstance(files, str):
+        files = [files]
+    if not isinstance(files, (list, tuple)):
+        raise TypeError("'files' must be a str or list of str's")
+    if not all([isinstance(s, str) for s in files]):
+        raise TypeError("all 'files' elements must be str's")
+
+    # Get configuration info
+    cDir = os.path.expanduser("~/pushPullConfig.csv")
+    msg = ("Missing or malformed ~/pushPullConfig.csv",
+           "Run:",
+           "source('https://raw.githubusercontent.com/hseltman/pushPull/" +
+           "master/setup.R'')",
+           "setup()",
+           "Try again after running setup.")
+
+    try:
+        with open(cDir) as fh:
+            config = fh.read()
+    except FileNotFoundError:
+        [print(m) for m in msg]
+        return "Failure"
+
+    config = [line.split(',') for line in config.splitlines()]
+    if not all(len(e) == 2 for e in config):
+        [print(m) for m in msg]
+        return "Failure"
+
+    config = {k.strip(): v.strip() for (k, v) in config}
+    needed = ('sftpSite', 'sftpName', 'sftpPassword', 'userName')
+    missing = [config.get(k) is None for k in needed]
+    if any(missing):
+        print("Missing config keys:",
+              ", ".join([n for (m, n) in zip(missing, needed) if m]))
+
+    # Handle the fact that it is possible that a named folder
+    # rather that the starting folder is the first writeable folder.
+    split = os.path.split(config['sftpSite'])
+    if len(split[0]) == 0:
+        site = split[1]
+        startDir = None
+    else:
+        site = split[0]
+        startDir = split[1]
+
+    # open the ftp site and change to the writeable folder
+    try:
+        cnopts = pysftp.CnOpts()
+        cnopts.hostkeys = None  # Hard to set on Windows
+        sftp = pysftp.Connection(host=site,
+                                 username=config['sftpName'],
+                                 password=config['sftpPassword'],
+                                 cnopts=cnopts)
+        try:
+            if startDir is not None:
+                sftp.cwd(startDir)
+        except FileNotFoundError:
+            print("the starting folder in 'sftpSite' is wrong")
+            return "Failure"
+    except pysftp.ConnectionException:
+        print("Cannot connect to {}".format(site))
+        return "Failure"
+    except pysftp.AuthenticationException:
+        print("Bad username or password in configuration file")
+        return "Failure"
 
     # pull the files
+    fail_count = 0
     for f in files:
         if who is None:
             try:
                 sftp.get(f)
             except FileNotFoundError:
                 print("'{}' was not found on the server".format(f))
+                fail_count += 1
         else:
             with sftp.cd():
-                sftp.cwd(who)
+                try:
+                    sftp.cwd(who)
+                except FileNotFoundError:
+                    print("'folder {}' is not on the server".format(who))
+                    fail_count += 1
                 try:
                     sftp.get(f)
                 except FileNotFoundError:
                     print("'{}' was not found on the server".format(f))
-    return None
+                    fail_count += 1
+    sftp.close()
 
-
-if __name__ == "__main__":
-    print(pull("aloha.txt", "hseltman"))
-
-if __name__ == "__testing__":
-    # Check if files exist
-    files = ["x"]
-    not_exists = [f for f in files if not os.path.exists(f)]
-    if len(not_exists) > 0:
-        raise Exception("file does not exist")
-
-    cnopts = pysftp.CnOpts()
-    cnopts.hostkeys = None
-    srv = pysftp.Connection(host="cetus.stat.cmu.edu",
-                            username="mspsftp",
-                            password="sftpmsp",
-                            cnopts=cnopts)
-    srv.cwd("mspsftp")
-    srv.get("empty.txt")
-    srv.put("ytpme.txt")
-    srv.cwd("hseltman")
-    srv.put("ytpme.txt")
-    srv.cwd("..")
-    srv.get("low.txt")
-    srv.listdir()
-    srv.get("hseltman/hello.txt")
-
-    with srv.cd():
-        srv.cwd("hseltman")
-        print(srv.listdir())
-        srv.put('README.md')
-    print(srv.listdir())
-
-    srv.close()
+    if fail_count == len(files):
+        return "Failed"
+    if fail_count == 0:
+        return "Success"
+    return "Partial success"
